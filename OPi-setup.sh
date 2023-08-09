7f664fb5d86aa3b07c4253a487fe7cad06f7a605
nightmac <chrisseithel@me.com>
2023-08-08 20:08:25 -0500

    Update OPi-setup.sh

--- OPi-setup.sh
+++ OPi-setup.sh
@@ -14,15 +14,13 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt -y dist-upgrade
 
-sudo apt install -y xfce4-goodies indicator-multiload
-
-# Installs Synaptic Package Manager for easy software install/removal
-echo "Installing Synaptic and apt-add-repository"
+echo "Installing Synaptic"
 sudo apt -y install synaptic software-properties-common
 
+sudo apt install -y xfce4-goodies indicator-multiload
 
 #########################################################
-#############  File Sharing Configuration
+#############  SAMBA
 
 echo "Setting up File Sharing"
 
@@ -57,16 +55,14 @@ EOF
 ##################
 fi
 
-# Adds yourself to the user group of who can use samba, but checks first if you are already in the list
 if [ -z "$(sudo pdbedit -L | grep $SUDO_USER)" ]
 then
 	sudo smbpasswd -a $SUDO_USER
 	sudo adduser $SUDO_USER sambashare
 fi
 
-
 #########################################################
-#############  Install and setup x11VNC
+#############  x11VNC
 
 sudo apt -y install x11vnc
 
@@ -96,36 +92,26 @@ sudo usermod -a -G dialout $SUDO_USER
 sudo apt -y remove brltty
 
 #########################################################
-#############  ASTRONOMY SOFTWARE
+#############  KStars INDI PHD2 Python3 INDIweb
 
-# Installs INDI, Kstars, and Ekos bleeding edge and debugging
 echo "Installing INDI and KStars"
 sudo apt-add-repository ppa:mutlaqja/ppa -y
 sudo apt update
 sudo apt -y install indi-full kstars-bleeding
 
-# Installs the General Star Catalog
 echo "Installing GSC"
 sudo apt -y install gsc
 
-display "Installing PHD2"
+echo "Installing PHD2"
 sudo apt-add-repository ppa:pch/phd2 -y
 sudo apt update
 sudo apt -y install phd2
 
-#########################################################
-#############  Python3 & INDIWEB
-
-echo "Installing python3 and indiweb"
-
+echo "Installing python3"
 sudo apt -y install python3-pip
 sudo apt -y install python3-dev
-
-# Setuptools may be needed in order to install indiweb on some systems
 sudo apt -y install python3-setuptools
 sudo -H -u $SUDO_USER pip3 install setuptools --upgrade
-
-# Wheel might not be installed on some systems
 sudo -H -u $SUDO_USER pip3 install wheel
 
 # This will install indiweb as the user
